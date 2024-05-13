0a4fa42ea9e0fa69adbf911b32b57c84050fbae9
Sebinmon V R <142289164+Reverse-Rain@users.noreply.github.com>
2024-05-13 20:06:57 +0530

    Update index.py

--- vision/index.py
+++ vision/index.py
@@ -1,1 +1,28 @@
 
+
+import requests
+
+url = "https://chatgpt-42.p.rapidapi.com/gpt4"
+
+payload = {
+	"messages": [
+		{
+			"role": "user",
+			"content": "hi"
+		}
+	],
+	"web_access": False
+}
+headers = {
+	"content-type": "application/json",
+	"X-RapidAPI-Key": "api",
+	"X-RapidAPI-Host": "chatgpt-42.p.rapidapi.com"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.json())
+
+
+
+
